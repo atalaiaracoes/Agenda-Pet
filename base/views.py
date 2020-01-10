5b7b0cf8f5bf17a8cd1d0996c20cec119ffae227
giba-atalaiaracoes <51959768+giba-atalaiaracoes@users.noreply.github.com>
2020-01-10 08:37:48 -0300

    Update views.py

--- base/views.py
+++ base/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.decorators import login_required
 
 
+
+@login_required(login_url='/')
 def index(request):
 	return render(request, 'inicial.html')
+
+def glogin(request):
+	return render(request, 'login.html')
+
+def glogout(request):
+	logout(request)
+	return redirect('/')
+
+@csrf_protect
+def submit(request):
+	username = request.POST.get('username')
+	password = request.POST.get('password')
+
+	user = authenticate(username=username, password=password)
+
+	if user is not None:
+		if user.is_active:
+			login(request,user)
+			return redirect('index')
+	else:
+		erro = "Usuário e/ou senha não cadastrados."
+		context = {'erro': erro}
+		return render(request, 'login.html', context)
+
+	return redirect('')
